045aa6e82507ea1f0856a7f8dc84154fbd4589e5
zanjie1999 <zanjie1999@yeah.net>
2021-05-11 17:33:50 +0800

    移动文件

--- build.bat
+++ build.bat
@@ -1,24 +1,21 @@
-del tcp2ws-windows.exe
-del tcp2ws-linux
-del tcp2ws-linux-arm64
-del kazari-linux.png
-del tcp2ws-darwin
+rd /s /q build
+mkdir build
 SET CGO_ENABLED=1
 SET GOARCH=amd64
 SET GOOS=windows
 go build
-rename tcp2ws.exe tcp2ws-windows.exe
+move tcp2ws.exe build\tcp2ws-windows.exe
 SET CGO_ENABLED=0
 SET GOOS=darwin
 go build
-rename tcp2ws tcp2ws-darwin
+move tcp2ws build\tcp2ws-darwin
 SET GOOS=linux
 go build
-rename tcp2ws tcp2ws-linux
+7z\7z a tcp2ws-linux.zip tcp2ws
+copy /b kazari.png+tcp2ws-linux.zip build\tcp2ws-zip-linux.png
+del tcp2ws-linux.zip
+move tcp2ws build\tcp2ws-linux
 SET GOARCH=arm64
 go build
-rename tcp2ws tcp2ws-linux-arm64
+move tcp2ws build\tcp2ws-linux-arm64
 
-7z a tcp2ws-linux.zip tcp2ws-linux
-copy /b kazari.png+tcp2ws-linux.zip tcp2ws-zip-linux.png
-del tcp2ws-linux.zip
