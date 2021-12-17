94b234bde950dc344795cffd2bcbf5b59566c088
zanjie1999 <zanjie1999@yeah.net>
2021-12-17 11:21:01 +0800

    mips arm i386

--- build.bat
+++ build.bat
@@ -5,19 +5,32 @@ SET GOARCH=amd64
 SET GOOS=windows
 go build
 move tcp2ws.exe build\tcp2ws.exe
-SET CGO_ENABLED=0
-SET GOOS=darwin
+SET GOARCH=386
 go build
-move tcp2ws build\tcp2ws-darwin
+move tcp2ws.exe build\tcp2ws-i386.exe
+SET CGO_ENABLED=0
 SET GOOS=linux
 go build
-7z\7z a tcp2ws-linux.zip tcp2ws
-copy /b kazari.png+tcp2ws-linux.zip build\tcp2ws-zip-linux.png
-del tcp2ws-linux.zip
+move tcp2ws build\tcp2ws-linux-i386
+SET GOARCH=amd64
+go build
+zip tcp2ws-linux.zip tcp2ws
+cp kazari.png build\tcp2ws-zip-linux.png
+cat tcp2ws-linux.zip >> build\tcp2ws-zip-linux.png
+rm tcp2ws-linux.zip
 move tcp2ws build\tcp2ws-linux
+SET GOARCH=arm
+go build
+move tcp2ws build\tcp2ws-linux-arm
+SET GOARCH=mips
+go build
+move tcp2ws build\tcp2ws-linux-mips
 SET GOARCH=arm64
 go build
 move tcp2ws build\tcp2ws-linux-arm64
 SET GOOS=darwin
 go build
 move tcp2ws build\tcp2ws-darwin-arm64
+SET GOARCH=amd64
+go build
+move tcp2ws build\tcp2ws-darwin
--- build.sh
+++ build.sh
@@ -1,25 +1,37 @@
 cd `dirname $0`
-rm -rf build
-mkdir build
+mkdir -p build
+rm -rf build/*
 export CGO_ENABLED=1
 export GOARCH=amd64
 export GOOS=windows
 go build
 mv tcp2ws.exe build/tcp2ws.exe
-export CGO_ENABLED=0
-export GOOS=darwin
+export GOARCH=386
 go build
-mv tcp2ws build/tcp2ws-darwin
+mv tcp2ws.exe build/tcp2ws-i386.exe
+export CGO_ENABLED=0
 export GOOS=linux
+go build
+mv tcp2ws build/tcp2ws-linux-i386
+export GOARCH=amd64
 go build
 zip tcp2ws-linux.zip tcp2ws
 cp kazari.png build/tcp2ws-zip-linux.png
 cat tcp2ws-linux.zip >> build/tcp2ws-zip-linux.png
 rm tcp2ws-linux.zip
 mv tcp2ws build/tcp2ws-linux
+export GOARCH=arm
+go build
+mv tcp2ws build/tcp2ws-linux-arm
+export GOARCH=mips
+go build
+mv tcp2ws build/tcp2ws-linux-mips
 export GOARCH=arm64
 go build
 mv tcp2ws build/tcp2ws-linux-arm64
 export GOOS=darwin
 go build
 mv tcp2ws build/tcp2ws-darwin-arm64
+export GOARCH=amd64
+go build
+mv tcp2ws build/tcp2ws-darwin
